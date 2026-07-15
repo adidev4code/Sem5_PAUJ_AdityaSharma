fbcab233a653316f5c90f3e0f6ef05f1fb298a0a
Aditya Sharma <adityaforwork09@gmail.com>
2026-07-15 15:48:08 +0530

    Create code.java

---
+++ 15 July 2026/code.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String args[]) {
+        MethodOverloading mo = new MethodOverloading();
+
+        // Method Overloading
+        System.out.println(mo.fullName("Aditya", "Sharma"));
+        System.out.println(mo.fullName("Aditya", "Vikram", "Sharma"));
+
+        // Method Overriding
+        Parent p = new Parent();
+        p.Overridingtext();
+
+        Child c = new Child();
+        c.Overridingtext();
+
+    }
+
+
+    public static class MethodOverloading
+    {
+        public String fullName(String fname, String lname)
+        {
+            return fname + " " + lname;
+        }
+
+        public String fullName(String fname, String mname, String lname)
+        {
+            return fname + " " + mname + " " + lname;
+        }
+    }
+
+    public static class Parent
+    {
+        public void Overridingtext(){
+            System.out.println("Hello from the Parent.");
+        }
+    }
+
+    public static class Child extends Parent
+    {
+        public void Overridingtext(){
+            System.out.println("Hello from the Child.");
+        }
+    }
+}
